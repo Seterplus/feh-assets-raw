1cfd262e87d126ef3409ecf3eba641e8f68d612a
Seter Plus <seter@pku.edu.cn>
2025-03-21 01:06:49 +0800

    Fix file name

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 mkdir out
 cp scripts/*.html out/
 uuid=$(python -c 'import uuid; print(str(uuid.uuid4()))')
-version=$(tail -c 8 scripts/dudu_update.bin)
+version=$(tail -c 8 scripts/update.bin)
 cp scripts/update.bin out/dudu_update_${uuid}.bin 
 sed -i -e "s/UUID/${uuid}/g" -e "s/DATE/${version}/g" out/index.html
 python scripts/arena.py
