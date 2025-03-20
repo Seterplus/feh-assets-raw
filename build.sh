103b8155636d10abc5f47d0b462159ad79e5e004
Seter Plus <seter@pku.edu.cn>
2025-03-21 01:05:18 +0800

    Fix UUID and font size

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 mkdir out
 cp scripts/*.html out/
-uuid=$(uuidgen)
+uuid=$(python -c 'import uuid; print(str(uuid.uuid4()))')
 version=$(tail -c 8 scripts/dudu_update.bin)
-cp scripts/dudu_update.bin out/dudu_update_${uuid}.bin 
+cp scripts/update.bin out/dudu_update_${uuid}.bin 
 sed -i -e "s/UUID/${uuid}/g" -e "s/DATE/${version}/g" out/index.html
 python scripts/arena.py
