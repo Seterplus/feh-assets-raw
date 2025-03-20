75a350e1c849fb44b7f295d3fe242a8a01035632
Seter Plus <seter@pku.edu.cn>
2025-03-21 00:58:17 +0800

    Serve FEH Rebuilder update

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 mkdir out
-cp scripts/arena.html out/
+cp scripts/*.html out/
+uuid=$(uuidgen)
+version=$(tail -c 8 scripts/dudu_update.bin)
+cp scripts/dudu_update.bin out/dudu_update_${uuid}.bin 
+sed -i -e "s/UUID/${uuid}/g" -e "s/DATE/${version}/g" out/index.html
 python scripts/arena.py
